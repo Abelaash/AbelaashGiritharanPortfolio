39a83b7f75a796afe7f8d7e4d3e2231e71da4b02
Abelaash Giritharan <50346809+Abelaash@users.noreply.github.com>
2020-10-13 17:30:05 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,10 +1,10 @@
 function opens(project, change) {
     var projOne = document.getElementById("projOne");
     var projTwo = document.getElementById("projTwo");
-   
+    var projThree = document.getElementById("projThree")
     projOne.style.display  = "none";
     projTwo.style.display = "none";
-  
+    projThree.style.display = "none";
 
     document.getElementById(project).style.display = "block";
 }
